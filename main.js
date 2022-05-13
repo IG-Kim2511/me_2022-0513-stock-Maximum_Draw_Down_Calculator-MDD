fadf57843fdd8d54d64212ad4c6e48566b435ddc
IG <iikim2511@gmail.com>
2022-05-13 16:56:01 -0400

    btnE.addEventListener('click',()=>{     console.log(nowE.value)  });

---
+++ main.js
@@ -0,0 +1,13 @@
+const wkHighE = document.querySelector('.wkHigh');
+const nowE = document.querySelector('.now');
+const resultE = document.querySelector('.result');
+const btnE = document.querySelector('.btn');
+
+
+//  = (52주 최고가 - 현재주가) / 52주 최고가 
+// const result = wkHighE.value
+
+btnE.addEventListener('click',()=>{
+    console.log(nowE.value)
+
+});
